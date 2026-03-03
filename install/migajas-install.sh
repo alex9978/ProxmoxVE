dfa8e630d3b2f6718324abb93edb4be5abf727cc
Alessandro Rasulo <alex9978@gmail.com>
2026-03-03 12:56:18 +0100

    fix: add openssl dependency for Alpine

--- install/migajas-install.sh
+++ install/migajas-install.sh
@@ -21,7 +21,8 @@ $STD apk add --no-cache \
   musl-dev \
   nginx \
   nodejs \
-  npm
+  npm \
+  openssl
 msg_ok "Installed Dependencies"
 
 GO_VERSION="$(curl -fsSL https://go.dev/VERSION?m=text | head -1 | cut -c3-)" setup_go
